5b8e6004f8f7b42fd46f64ff3140c2125cf9f3fa
mikemel21 <mikemelei905@gmail.com>
2024-11-15 20:16:41 -0600

    transparent background

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -20,16 +20,16 @@ return {
             {'rafamadriz/friendly-snippets'}, -- Optional
         },
   },
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-    }
-  },
+--  {
+--    "hrsh7th/cmp-nvim-lsp"
+--  },
+--  {
+--    "L3MON4D3/LuaSnip",
+--    dependencies = {
+--      "saadparwaiz1/cmp_luasnip",
+--      "rafamadriz/friendly-snippets"
+--    }
+--  },
   {
     "hrsh7th/nvim-cmp",
     config = function()
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,18 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.g.gruvbox_transparent_bg = 1
+
+-- Transparent background
+vim.cmd[[
+  augroup TransparentBackground
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+    autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+  augroup END
+]]
+
 vim.g.mapleader = " "
 
 -- line numbers
