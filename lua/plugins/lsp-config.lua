154dc286041d570a77ea03106587b8b1feeb0527
mikemel21 <mikemelei905@gmail.com>
2025-02-25 19:53:50 -0600

    emmet

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
  
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
         settings = {
@@ -32,12 +33,26 @@ return {
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       }) -- JS and TS
-      lspconfig.ltex.setup({
-        capabilities = capabilities
-      }) -- MD, LaTex, etc.
+--      lspconfig.ltex.setup({
+--        capabilities = capabilities
+--      }) -- MD, LaTex, etc.
       lspconfig.pyright.setup({
         capabilities = capabilities
       }) -- Python (might work)
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        file_types = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"},
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+            }
+          }
+        }
+      })
 
       -- display more info of piece of code
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
